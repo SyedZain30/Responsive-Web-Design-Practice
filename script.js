60dbd3f1a9b97171b275e9f2191a7fcac72b8348
SyedZain30 <88159703+SyedZain30@users.noreply.github.com>
2024-09-12 20:27:20 +0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-let menu = document.querySelector('#menu-icon'); // Corrected selector
-let navlist = document.querySelector('.navlist');
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navlist.classList.toggle('open');
-};
